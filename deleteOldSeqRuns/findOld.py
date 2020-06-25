81d8c42c3bad5726658c98841b83f8c79c33b83f
Anders Lind <mail@anderslind.se>
2020-06-25 17:40:18 +0200

    Can now get a list of who owns what sample

--- deleteOldSeqRuns/findOld.py
+++ deleteOldSeqRuns/findOld.py
@@ -12,14 +12,16 @@ import os
 import click
 from datetime import date
 import csv
+from collections import defaultdict
 
 # Get arguments from the commandline
 @click.command()
 @click.option('-a', '--age', type=int, required=True, help='Age of dirs, in days, to be deleted')
 @click.option('-d', '--directory', required=True, help='Path to directories')
-#@click.option('-o','--owners', required=True, help='csv file of who owns what directory')
-def main (age, directory):
+@click.option('-s', '--sheetname', help='Name of file with ownership info of samples. default: "SampleSheet.csv')
+def main (age, directory, sheetname='SampleSheet'):
     #Get a list of all seq runs in directory
+    sheetname = 'SampleSheet_exempel.csv'  #For testing purposes only, remove this
     runs = []
     for path in os.listdir(directory):  #This is a kinda ugly way to get all the dirs
         #bug(path)
@@ -30,15 +32,20 @@ def main (age, directory):
 
     #Go over each sequencing run one by one
     for run in runs:
-        bug(run)
+        #bug(run)
         # Get a list of samples to be removed
-        to_remove = getOld(age, run)
-        # See who owes each samples that should be removed
+        to_remove = getOld(age, run)  #Paths to all samples
+        to_remove_samples = list(map(lambda str: str.split('/')[-1], to_remove))  #List of the samples themselves
+
+        #E-mail the people who have old samples
+        # First make sure the SampleSheet.csv file exists
+        if not os.path.exists(os.path.join(run, sheetname)):
+            print("** ERROR: No " + sheetname + " @ " + run)
+        # See who owes each sample in the run
+        to_email = dirOwners(os.path.join(run, sheetname), to_remove_samples)
+        bug(to_email)
 
 
-    #Get the ownership information for all samples to be removed
-    #to_email = dirOwners(owners)
-    #bug(to_email)
 
 def getOld (age, run):  #Check which folders are older than age
     today = date.today()
@@ -50,15 +57,24 @@ def getOld (age, run):  #Check which folders are older than age
             if (today - filedate).days > age:
                 old_dirs.append(full_name)
     return old_dirs
-#
-# def dirOwners (owners): #Who owns what folder
-#     with open(owners, mode='r') as csv_file:
-#         csv_reader = csv.reader(csv_file)
-#         for row in csv_reader:
-#             owner_dict = {row[0]:[row[1],row[2]]}
-#             bug(owner_dict)
-#
-#     return owners
+
+def dirOwners (run, samples): #Who owns what sample
+    #bug(samples)
+    with open(run, mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        pairs = []
+        # Read in each line and save info about sample and owner
+        for row in csv_reader:
+            if row and row[0] in samples:  #skip empty rows and rows without sample info
+                extract = [row[10].split('_')[0], row[0]]
+                pairs.append(extract)
+
+   # Load values into a dictionary
+    owner_dict = defaultdict(list)
+    for key, value in pairs:
+        owner_dict[key].append(value)
+
+    return owner_dict
 
 
 
