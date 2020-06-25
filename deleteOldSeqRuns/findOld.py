7f255159ff70c7e585daf671b710c17ec6819214
Anders Lind <mail@anderslind.se>
2020-06-25 14:02:53 +0200

    Function to find folders older than given date done

--- deleteOldSeqRuns/findOld.py
+++ deleteOldSeqRuns/findOld.py
@@ -10,12 +10,25 @@ Will also first send out an email to the owner to warn
 #Import libraries
 import os
 import click
+from datetime import date
 
 # Get arguments from the commandline
 @click.command()
-def main ():
+@click.option('-a', '--age', type=int, required=True, help='Age of dirs, in days, to be deleted')
+@click.option('-d', '--directory', required=True, help='Path to directories')
+def main (age, directory):
+    to_remove = getOld(age, directory) #Get list of folders to delete
+    print(to_remove)
 
-
+def getOld (age, directory): #Check which folders are older than age
+    today = date.today()
+    old_dirs = []
+    for name in os.listdir(directory):
+        dirPath = directory + name
+        filedate = date.fromtimestamp(os.path.getmtime(dirPath))
+        if (today - filedate).days > age:
+            old_dirs.append(dirPath)
+    return old_dirs
 
 if __name__ == '__main__':
     main()
