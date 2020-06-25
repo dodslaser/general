c8b879e07731e565e46cb90576b6d4aa3ed7f931
Anders Lind <mail@anderslind.se>
2020-06-25 17:50:06 +0200

    Added the emailSender.py file

---
+++ deleteOldSeqRuns/emailSender.py
@@ -0,0 +1,14 @@
+
+text = """\
+Hej.
+
+Detta är ett automatiskt mail som skickas då ni står som ägare för rå-sekvensdata som snart kommer att tas bort från filservern permanent.
+
+Dessa kommer automatiskt att tas bort om 10 dagar. Om ni inte har något behov av att spara dessa filer så behöver ni inte göra någonting. Om ni istället vill att vi spar dessa så kontakta oss så snart som möjligt.
+
+De prover som berörs är från körningen med ID <run name> och har följande sample IDn:
+<samples list>
+
+Med vänliga automatiska hälsningar,
+CGG Bioinformatik
+"""
