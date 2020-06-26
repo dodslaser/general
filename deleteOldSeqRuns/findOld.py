c6c9846922030442c5565033d6139e6a87ae2606
Anders Lind <mail@anderslind.se>
2020-06-26 16:41:38 +0200

    Changed so age of run is taken from directory

--- deleteOldSeqRuns/findOld.py
+++ deleteOldSeqRuns/findOld.py
@@ -10,58 +10,111 @@ Will also first send out an email to the owner to warn
 #Import libraries
 import os
 import click
-from datetime import date
+import datetime
 import csv
 from collections import defaultdict
 import json
+import re
 
 # Get arguments from the commandline
 @click.command()
-@click.option('-a', '--age', type=int, required=True, help='Age of dirs, in days, to be deleted')
-@click.option('-d', '--directory', required=True, help='Path to directories')
-@click.option('-s', '--sheetname', help='Name of file with ownership info of samples. default: "SampleSheet.csv')
-@click.option('-i', '--investigatorlist', required=True, help='Path to json file with investigator e-mails')
-def main (age, directory, investigatorlist, sheetname='SampleSheet_exempel.csv'):
+@click.option('-a', '--age', type=int, required=True,
+              help='Age of dirs, in days, to be deleted')
+@click.option('-d', '--directory', required=True,
+              help='Path to directories')
+@click.option('-s', '--sheetname', default="SampleSheet.csv",
+              help='Name of file with ownership info of samples. default: "SampleSheet.csv')
+@click.option('-i', '--investigatorlist', required=True,
+              help='Path to json file with investigator e-mails')
+def main (age, directory, investigatorlist, sheetname):
+    # First get a list of what e-mail belongs to what person
+    with open(investigatorlist) as f:
+        data = json.load(f)
+
     #Get a list of all seq runs in directory
-    sheetname = 'SampleSheet_exempel.csv'  #For testing purposes only, remove this
+    #sheetname = 'SampleSheet.csv'  #For testing purposes only, remove this
     runs = []
+
     for path in os.listdir(directory):  #This is a kinda ugly way to get all the dirs
         #bug(path)
         full_path = os.path.join(directory, path)
-        if os.path.isdir(full_path):
+        if os.path.isdir(full_path) and re.match("^[0-9]{6}_", path):  #Is dir and starts with 6 digits
             #bug(full_path)
-            runs.append(full_path)
+            runs.append(path)
+            #runs.append(full_path)
 
     #Go over each sequencing run one by one
+    #owner_dict = {}
+
     for run in runs:
+        #Full path of the run
+        run_path = os.path.join(directory, run)
         #bug(run)
-        # Get a list of samples to be removed
-        to_remove = getOld(age, run)  #Paths to all samples
+        # What is the age of the run
+        owner_dict = runAge(age, run)
+        #bug(owner_dict.keys())
+
+        #What samples are in each run
+        bug(run_path)
+        run_samples = runSamples(run_path)
+        bug(run_samples)
+
+        to_remove = getOld(age, run_path)  #Paths to all samples
         to_remove_samples = list(map(lambda str: str.split('/')[-1], to_remove))  #List of the samples themselves
         #bug(to_remove_samples)
         #E-mail the people who have old samples
         # First make sure the SampleSheet.csv file exists
-        if not os.path.exists(os.path.join(run, sheetname)):
+        if not os.path.exists(os.path.join(run_path, sheetname)):
             print("** ERROR: No " + sheetname + " @ " + run)
-        # See who owes each sample in the run
-        to_email = dirOwners(os.path.join(run, sheetname), to_remove_samples)
-        #bug(to_email)
+        else:
+            # See who owes each sample in the run
+            to_email = dirOwners(os.path.join(run_path, sheetname), to_remove_samples)
+            #bug(to_email)
 
     # Send an email to the owners
-    # First get a list of what e-mail belongs to what person
-    with open(investigatorlist) as f:
-        data = json.load(f)
+
     #bug(data['investigators']['CO']['email'])
     #autoMail(run, to_remove_samples, address)
 
+def runAge (age, run):
+    today = datetime.datetime.now()
+    run_dict = {}
+
+    run_date = datetime.datetime.strptime(run.split('_')[0], '%y%m%d')
+    run_age = (today - run_date).days
+
+    if run_age > age:
+        run_dict[run] = {}
+        run_dict[run]['age'] = run_age
+
+        #bug(run_dict)
+
+    return run_dict
+
+def runSamples (run):
+    samples = []
+    for name in os.listdir(run):
+        #bug(name)
+        if os.path.isdir(os.path.join(run, name)):
+            #bug(name)
+            samples.append(name)
+
+    return samples
+
+
+
 def getOld (age, run):  #Check which folders are older than age, return name and age
-    today = date.today()
-    #old_dirs = [] #Initial list based way
+    today = datetime.date.today()
+    #old_dirs = [] #Initial, list based way
     old_dirs = {}
     for name in os.listdir(run):
-        full_name = os.path.join(run,name)
+        #bug(name)
+        full_name = os.path.join(run, name)
         if os.path.isdir(full_name):
-            filedate = date.fromtimestamp(os.path.getmtime(full_name))
+            filedate = datetime.date.fromtimestamp(os.path.getmtime(full_name))
+            #bug("hej:")
+            #bug(filedate)
+
             age_today = (today - filedate).days
             if age_today > age:
                 old_dirs[name] = {'path': full_name, 'age': age_today}
@@ -69,7 +122,7 @@ def getOld (age, run):  #Check which folders are older than age, return name and
     #bug(old_dirs)
     return old_dirs
 
-def dirOwners (run, samples): #Who owns what sample
+def dirOwners (run, samples):  #Who owns what sample
     #bug(samples)
     with open(run, mode='r') as csv_file:
         csv_reader = csv.reader(csv_file)
