eca3d097520a6ef642da594f209fb0b3c8c67afe
Anders Lind <mail@anderslind.se>
2021-01-21 12:19:47 +0100

    Added README

---
+++ sprintName/README.py
@@ -0,0 +1,6 @@
+# sprintName
+
+This small script just creates alliterative names based on the adjective-animal.
+It can be given a starting letter, or just randomly choose one.
+
+When a name has been selected, both the adjective and animal name is being kept tracked of, so they won't come up again.
