ea64fa1309a86623209c5eccbc0f2e1ba01a4bf5
Anders Lind <mail@anderslind.se>
2022-01-21 14:02:18 +0100

    Removed all old linkFastq stuff and added initial checks

--- move_researchdata/move_researchdata.py
+++ move_researchdata/move_researchdata.py
@@ -1,80 +1,42 @@
 #!/usr/bin/env python
-
+import sys
 import click
+from sample_sheet import SampleSheet
 import os
-import csv
-from itertools import dropwhile
-import glob
+import logging
+
 
 @click.command()
 @click.option('-d', '--demultiplexdir', required=True,
               help='Path to demultiplex dir of run')
-@click.option('-o', '--outdir', required=True,
-              help='Path to output folder')
-def main(demultiplexdir, outdir):
-    #Set the path to the samplesheet
-    samplesheet = os.path.join(demultiplexdir, 'SampleSheet.csv')
-    
-    #Collect which samples were found
-    sample_list = []
+def main(demultiplexdir):
+    # Set up the logfile and start logging
+    logger = setup_logger('mv_resproj')
+    logger.info(f'Looking for data belonging to research projects in {demultiplexdir}.')
 
-    #Loop over the sample sheet and find all Pathology samples
-    with open(samplesheet, mode='r') as f:
-        csv_reader = csv.reader(f)
-        next(dropwhile(isDataLine, csv_reader)) # Skip until the line starts with [Data]
-        for row in csv_reader:
-            department = row[9]
-            #Choose only samples belonging to PAT
-            if department == 'PAT':
-                sample = row[1]
-                #Append sample to sample list
-                sample_list.append(sample)
+    # Check that the demultiplexdir exists
+    if not os.path.exists(demultiplexdir):
+        logger.error(f"The path {demultiplexdir} does not seem to exist.")
+        sys.exit()
 
-    #Write some logging
-    print("** LOG: Found the following samples belonging to PAT:")
-    for sample in sample_list:
-        print("\t" + sample)
-    print ("** LOG: Linking fastq files...", end="")
-
-    #Make the symlinks for the samples
-    for sample in sample_list:
-        fastqs = allFastqs(demultiplexdir, sample)
-        for fastq in fastqs:
-            destPath = os.path.join(outdir, os.path.basename(fastq))
-            if os.path.exists(destPath): #Skip if already there
-                next
-            else:
-                os.symlink(fastq, destPath)
-    print("done.")
-
-    #Link the fastQC report
-    print ("** LOG: Linking fastqc reports...", end="")
-    for sample in sample_list:
-        QCs = allQC(demultiplexdir, sample)
-        for QC in QCs:
-            destPath = os.path.join(outdir, os.path.basename(QC))
-            if os.path.exists(destPath): #Skip if already there
-                next
-            else:
-                os.symlink(QC, destPath)
-    print("done.")
-
-def isDataLine(line):
-    if line and line[0] == 'Sample_ID':
-        return False
+    # Look for path to SampleSheet
+    samplesheet_path = os.path.join(demultiplexdir, 'SampleSheet.csv')
+    if os.path.exists(samplesheet_path):
+        print("Found it")
     else:
-        return True
+        logger.error(f'Could not SampleSheet.csv @ {demultiplexdir}')
+        sys.exit()
 
-def allFastqs(demultiplexdir, sample):
-    fastqPath = demultiplexdir + "/fastq/" + sample + "*.fastq.gz"
-    fastqList = glob.glob(fastqPath)
-    return fastqList
+def setup_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
 
-def allQC(demultiplexdir, sample):
-    fastqcPath = demultiplexdir + "/fastqc/" + sample + "*_fastqc.html"
-    QCList = glob.glob(fastqcPath)
-    return QCList
+    stream_handle = logging.StreamHandler()
+    stream_handle.setLevel(logging.DEBUG)
+    stream_handle.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    logger.addHandler(stream_handle)
 
+    return logger
 
 if __name__ == '__main__':
     main()
