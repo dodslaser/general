21f238e3faf74fec5656ff07ee7c2cec169abbe4
Anders Lind <mail@anderslind.se>
2022-02-28 10:16:09 +0100

    Added some arg parsing

--- sftp/make_sftp_user.sh
+++ sftp/make_sftp_user.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-#Check that the script is executed on seqstore, otherwise quit
+#Usage information
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $(basename $0) <USERID> <ROOT FOLDER>"
+    exit 1
+fi
+
+#Check that the script is executed on seqstore and as root, otherwise quit
 if [ "$(hostname)" != "seqstore.medair.lcl" ]; then
   echo "This script needs to be executed on seqstore. Quitting."
   exit 1
 fi
+if [ "$(whoami)" != "root" ]; then
+  echo "You need to execute this as root. Quitting"
+  exit 1
+fi
+
+#Get some ARG vars
+USERID=$1
+ROOTFOLDER=$2
 
