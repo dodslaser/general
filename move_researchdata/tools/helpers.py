f4232524baee33f3d1573ec51e63e6a872415a5c
Anders Lind <mail@anderslind.se>
2022-03-28 14:23:53 +0200

    Added look_for_runs from WOPR wrapper

--- move_researchdata/tools/helpers.py
+++ move_researchdata/tools/helpers.py
@@ -10,3 +10,9 @@ def setup_logger(name):
     logger.addHandler(stream_handle)
 
     return logger
+
+def look_for_runs(root_path):
+    found_paths = glob.glob(os.path.join(root_path, '*'))
+    regex = '^[0-9]{6}_A[0-9]{5}_[0-9]{4}_.{10}$'
+    return [path for path in found_paths if re.search(regex, os.path.basename(path))]
+
