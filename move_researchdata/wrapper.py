2b5cf7e574e4313ca3a3b02b37f28c16e83e9ced
Anders Lind <mail@anderslind.se>
2022-03-31 10:25:22 +0200

    Fixed relative paths causing problems

--- move_researchdata/wrapper.py
+++ move_researchdata/wrapper.py
@@ -9,8 +9,10 @@ import sys
 sys.path.append("..")
 from CGG.tools.emailer import send_email
 
+wrapper_path = os.path.abspath(os.path.dirname(__file__))
+
 @click.command()
-@click.option('--config-path', default='configs/wrapper_config.yaml',
+@click.option('--config-path', default=os.path.join(wrapper_path, 'configs/wrapper_config.yaml'),
               type=click.Path(exists=True), help='Path to wrapper config file')
 def wrapper(config_path):
     ## Sanity check. Only run as root
@@ -27,7 +29,7 @@ def wrapper(config_path):
     logger = setup_logger('wrapper', full_log_path)
 
     ## Read in demuxdir-runlist.txt
-    runlist = config['previous_runs_file_path']
+    runlist = os.path.join(wrapper_path, config['previous_runs_file_path'])
     with open(runlist, 'r') as prev:
         previous_runs = [line.rstrip() for line in prev]
 
